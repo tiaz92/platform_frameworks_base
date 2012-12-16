3361752e496a535f2aaffa65cabbfe622cd11581
László Dávid <laszlo.david@gmail.com>
2012-12-16 21:57:16 +0100

    Status bar brightness control
    Change-Id: Ibc5cc15a9b43eade73670d308d5cc788c93b6210

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2612,6 +2612,13 @@ public final class Settings {
         public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
 
         /**
+        * Whether to control brightness from status bar
+        *
+        * @hide
+        */
+       public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
