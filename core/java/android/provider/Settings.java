fb34145618982dee7ea223f33b09f68188eb51a6
Danny Baumann <dannybaumann@web.de>
2013-11-09 13:10:15 +0100

    Navigation ring customization.
    Change-Id: Iebe4ec7c2519b85cfd5189d060444764a50f3cf6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2862,6 +2862,17 @@ public final class Settings {
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
         /**
+         * Custom navring actions
+         *
+         * @hide
+         */
+        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
+            "navigation_ring_targets_0",
+            "navigation_ring_targets_1",
+            "navigation_ring_targets_2",
+        };
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
