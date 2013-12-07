0b843fd4be7eb866073904d1d5745906346a4036
Jorge Ruesga <jorge@ruesga.com>
2013-12-08 00:20:03 +0100

    Squashed: Add dock battery support
    Dock battery patchset chain:
    base: http://review.cyanogenmod.org/#/c/55349/
    native: http://review.cyanogenmod.org/#/c/55028/
    healthd: http://review.cyanogenmod.org/#/c/55029/
    settings: http://review.cyanogenmod.org/#/c/55212/
    tf700t: http://review.cyanogenmod.org/#/c/55030/
    
    Video (battery styles): https://www.dropbox.com/s/a7dp3glb7lv1zcg/dock_sdcard01.mp4
    Screenshots:
     * No dock: https://www.dropbox.com/s/2yvngdaqok32xiz/dock_sdcard01.png?m=
     * No charging: https://www.dropbox.com/s/ezghtgoedei17oj/dock_sdcard02.png?m=
     * Docked no charging: https://www.dropbox.com/s/7fasc05fw5ylfxi/dock_sdcard03.png?m=
     * Docked charging: https://www.dropbox.com/s/x37vrhxzd075f7p/dock_sdcard04.png?m=
    
    Fix landscape battery:
    https://www.dropbox.com/s/nlufek6gmwrjvyn/dock_fix1.png
    https://www.dropbox.com/s/yo5y026o9piwil8/dock_fix2.png
    
    Fix: battery history graph
    https://www.dropbox.com/s/jkf7uioupyl05x7/Screenshot_2013-12-12-04-03-38.png
    
    Change-Id: Ia36dbc5c76bf5e256d660e4594ca1169eeb3f00b
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	packages/SettingsProvider/res/values/defaults.xml
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
    	packages/SystemUI/res/values/cm_arrays.xml
    	packages/SystemUI/res/values/purity_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    	services/java/com/android/server/BatteryService.java
    
    Unbreak the public API again...
    
    Change-Id: I899142cec4084cead1d730328132a49e1dafe6f9
    
    systemui: Fix hidden battery icon style
    
    Change-Id: I52a13f4945e2ea654221f73ca1357dc72b4f4cf9
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    systemui: fix circle battery percentage alignment for low dpi devices
    
    This changes replaces the +1px for +1dp for a better center
    text inside the circle battery
    
    Change-Id: I71d5e20c20f3125b5affe4fa93986ae4d12a61a1
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    systemui: Use the same color for circle battery than clock color
    
    Patchset 3: Fix code derp
    
    Change-Id: Iaa7b778b705468996079fe600821eeb94a6ecb28
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    BatteryService: Restore the original parcel order
    
    Append the dock extensions to the end of the parcel to preserve
    compatibility with the original parcel format.
    
    Change-Id: I92112e244ab85d6c93e8e5ebf37de8257af6413a
    
    Fix charging indication.
    
    This commit fixes a number of issues in the new battery drawing code:
    - Use the BatteryTracker instances consistently
    - Don't indicate charging (by displaying the bolt or the circle
      animation) if the device is connected to a charger, but not actually
      charging, e.g. because of the charger not providing a sufficient
      amount of current.
    - Fix some style issues (variable naming)
    
    Change-Id: I957c8c525310170a3d4480047fbf6e92533c0296
    
    Fix derp
    
    yeah

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -67,6 +67,7 @@ import com.android.systemui.quicksettings.BluetoothTile;
 import com.android.systemui.quicksettings.BrightnessTile;
 import com.android.systemui.quicksettings.BugReportTile;
 import com.android.systemui.quicksettings.CameraTile;
+import com.android.systemui.quicksettings.DockBatteryTile;
 import com.android.systemui.quicksettings.ExpandedDesktopTile;
 import com.android.systemui.quicksettings.GPSTile;
 import com.android.systemui.quicksettings.InputMethodTile;
@@ -141,6 +142,10 @@ public class QuickSettingsController {
         mQuickSettingsTiles = new ArrayList<QuickSettingsTile>();
         mSettingsKey = settingsKey;
         mRibbonMode = ribbonMode;
+    }
+
+    public boolean isRibbonMode() {
+        return mRibbonMode;
     }
 
     void loadTiles() {
@@ -253,10 +258,10 @@ public class QuickSettingsController {
                 mQuickSettingsTiles.add(qs);
 
                 // Add dock battery beside main battery when possible
-                /*if (qs instanceof BatteryTile) {
+                if (qs instanceof BatteryTile) {
                     loadDockBatteryTile(resolver, inflater);
                     dockBatteryLoaded = true;
-                }*/
+                }
             }
         }
 
@@ -302,18 +307,20 @@ public class QuickSettingsController {
         }
     }
 
-    private void loadDockBatteryTile(final ContentResolver resolver, final LayoutInflater inflater) {
+    private void loadDockBatteryTile(final ContentResolver resolver,
+            final LayoutInflater inflater) {
         if (!QSUtils.deviceSupportsDockBattery(mContext)) {
             return;
         }
-        /*if (Settings.System.getIntForUser(resolver,
+        if (Settings.System.getIntForUser(resolver,
                     Settings.System.QS_DYNAMIC_DOCK_BATTERY, 1, UserHandle.USER_CURRENT) == 0) {
             return;
-        }*/
+        }
 
-  /*      QuickSettingsTile qs = new DockBatteryTile(mContext, this, mStatusBarService.mDockBatteryController);
+        QuickSettingsTile qs = new DockBatteryTile(mContext, this,
+                mStatusBarService.mDockBatteryController);
         qs.setupQuickSettingsTile(inflater, mContainerView);
-        mQuickSettingsTiles.add(qs);*/
+        mQuickSettingsTiles.add(qs);
     }
 
     public void shutdown() {
