CyanogenMod/android_frameworks_base

QS: Make it possible to show smaller icons (1/2)
* Remove text and show more icons on one row

Change-Id: Ie247aba1ae5fbdb8f5298e30fb2f04a985e73532

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3093,6 +3093,14 @@ public class PhoneStatusBar extends BaseStatusBar {
                     cleanupRibbon();
                     inflateRibbon();
                     mRibbonView.setVisibility(View.VISIBLE);
+             
+            } else if (uri != null && uri.equals(Settings.System.getUriFor(
+                    Settings.System.QUICK_SETTINGS_SHOW_LABELS))) {
+                if (mSettingsContainer != null) {
+                    mQS.setupQuickSettings();
+                    mSettingsContainer.updateResources();
+                }
+            
             } else if (mSettingsContainer != null) {
                 mQS.setupQuickSettings();
                 if (mQuickAccessLayoutLinked && mRibbonQS != null) {
@@ -3142,6 +3150,11 @@ public class PhoneStatusBar extends BaseStatusBar {
             cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.QUICK_SETTINGS_RIBBON_TILES),
                     false, this, UserHandle.USER_ALL);
+
+            cr.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.QUICK_SETTINGS_SHOW_LABELS),
+                    false, this, UserHandle.USER_ALL);
+
         }
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -348,7 +348,10 @@ public class QuickSettingsController {
         loadTiles();
         setupBroadcastReceiver();
         setupContentObserver();
-        if (mHideLiveTileLabels) {
+        ContentResolver resolver = mContext.getContentResolver();
+        boolean showLabels = Settings.System.getIntForUser(resolver,
+                Settings.System.QUICK_SETTINGS_SHOW_LABELS, 1, UserHandle.USER_CURRENT) == 1;
+        if (mHideLiveTileLabels || !showLabels) {
             for (QuickSettingsTile t : mQuickSettingsTiles) {
                 t.setLabelVisibility(false);
             }

tiaz92/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3227,6 +3227,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                     cleanupRibbon();
                     inflateRibbon();
                     mRibbonView.setVisibility(View.VISIBLE);
+            } else if (uri != null && uri.equals(Settings.System.getUriFor(
+                    Settings.System.QUICK_SETTINGS_HIDE_LABELS))) {
+                if (mSettingsContainer != null) {
+                    mQS.setupQuickSettings();
+                    mSettingsContainer.updateResources();
+                }
             } else if (mSettingsContainer != null) {
                 mQS.setupQuickSettings();
                 if (mQuickAccessLayoutLinked && mRibbonQS != null) {
@@ -3276,6 +3282,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.QUICK_SETTINGS_RIBBON_TILES),
                     false, this, UserHandle.USER_ALL);
+
+            cr.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.QUICK_SETTINGS_HIDE_LABELS),
+                    false, this, UserHandle.USER_ALL);
+
         }
     }
 } 

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -333,7 +333,10 @@ public class QuickSettingsController {
         loadTiles();
         setupBroadcastReceiver();
         setupContentObserver();
-        if (mRibbonMode) {
+        ContentResolver resolver = mContext.getContentResolver();
+        boolean showLabels = Settings.System.getIntForUser(resolver,
+                Settings.System.QUICK_SETTINGS_HIDE_LABELS, 0, UserHandle.USER_CURRENT) == 0;
+        if (mRibbonMode || !showLabels) {
             for (QuickSettingsTile t : mQuickSettingsTiles) {
                 t.switchToRibbonMode();
             }